d7a084094e5b4308a0f5b3a8d6cb33da8289edff
Christian Dimitri <chriistian.j.dimitrii@gmail.com>
2022-01-28 20:18:02 +0100

    Dropper class added, still in progress

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Dropper : MonoBehaviour
+    {
+        [SerializeField] private float timeToWait = 5f;
+
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+            if (Time.time > timeToWait)
+            {
+                Debug.Log("5 seconds have passed");
+            }
+        }
+    }
+}
